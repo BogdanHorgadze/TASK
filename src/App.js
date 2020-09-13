e46e6c981a57f05f67abc3cd1823ab981b8e5f96
bogdan <bogdangorgadze1@gmail.com>
2020-09-13 19:17:54 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
+import {add} from './store/actions/action'
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{props.count}</div>
+       <button onClick={props.addCount}>+</button>
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state){
+  return {
+    count : state.randomReducer.count
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addCount : () => dispatch(add())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducers/rootReducer'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+      <App />
+  </Provider>,
   document.getElementById('root')
 );
 
